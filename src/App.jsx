448b8e89443aee086d9598432fdc48103e594d1a
br08 <brayan.neves@gmail.com>
2023-04-27 17:18:29 -0300

    added MainHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import MainHeader from "./components/MainHeader";
+
 function App() {
   return (
     <>
+      <MainHeader onCreatePost={showModalHandler} />
       <main>
         <h1>Hello, there!</h1>
       </main>
